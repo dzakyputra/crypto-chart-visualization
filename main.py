293673dbb09c8fc405f2955e3cca8a9225bbf06e
Dzaky Widya Putra <dzakywidyaputra@gmail.com>
2023-02-07 16:00:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -61,4 +61,4 @@ def info():
     return send_file(image_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
